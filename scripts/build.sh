ce1a075c6f9f30141bd4e9937aaef496a3e4ff53
Michael Vilim <michael.vilim@gmail.com>
2019-01-30 16:30:55 -0600

    Only build sdist on linux

---
+++ scripts/build.sh
@@ -0,0 +1,9 @@
+# temporary fix for https://github.com/joerick/cibuildwheel/issues/122
+# pip install cibuildwheel==0.10.0
+pip install git+https://github.com/YannickJadoul/cibuildwheel.git@pip-19-stalling-workaround
+cibuildwheel --output-dir dist
+
+# only create the source distribution from linux, so that we don't try to upload it twice
+if [ "$TRAVIS_OS_NAME" = "linux" ]
+    python setup.py sdist --dist-dir dist
+fi
