57ff07421381442430913490e7ec954d15007a3d
Michael Vilim <michael.vilim@gmail.com>
2019-02-01 12:33:29 -0600

    Bump version: 0.1.17 → 0.2.0

--- setup.py
+++ setup.py
@@ -111,7 +111,7 @@ with open(path.join(path.abspath(path.dirname(__file__)), 'README.md')) as fh:
 
 setup(
     name='pylibtermkey',
-    version='0.1.17',
+    version='0.2.0',
     author='Michael Vilim',
     author_email='michael.vilim@gmail.com',
     url='https://github.com/mvilim/pylibtermkey',
