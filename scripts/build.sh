a7227d59f0689c9cd3419086bfc9b284a7b53a9f
Michael Vilim <michael.vilim@gmail.com>
2019-03-08 10:04:14 -0600

    Change build script for travis osx

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,12 +1,19 @@
 set -e -x
 
-# temporary fix for https://github.com/joerick/cibuildwheel/issues/122
-# pip install cibuildwheel==0.10.0
-pip install git+https://github.com/YannickJadoul/cibuildwheel.git@pip-19-stalling-workaround
+CIBUILDWHEEL_VERSION=0.10.1
+
+if [ "$TRAVIS_OS_NAME" = "linux" ]; then
+    pip install cibuildwheel=$CIBUILDWHEEL_VERSION
+elif [ "$TRAVIS_OS_NAME" = "osx" ]; then
+    sudo pip2 install cibuildwheel==$CIBUILDWHEEL_VERSION
+else
+    echo Unrecognized OS
+    exit -1
+fi
+
 cibuildwheel --output-dir dist
 
 # only create the source distribution from linux, so that we don't try to upload it twice
-if [ "$TRAVIS_OS_NAME" = "linux" ]
-then
+if [ "$TRAVIS_OS_NAME" = "linux" ]; then
     python setup.py sdist --dist-dir dist
 fi
