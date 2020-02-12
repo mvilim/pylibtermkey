08ba3aaf0407efe3914b4bd04c257434274f5491
Michael Vilim <michael.vilim@gmail.com>
2020-02-11 20:19:06 -0600

    Try another curses package

--- scripts/travis_build.sh
+++ scripts/travis_build.sh
@@ -1,7 +1,7 @@
 set -e -x
 
 if [ "$TRAVIS_OS_NAME" = "linux" ]; then
-    sudo apt install -y libncurses-dev
+    sudo apt install -y libncurses5-dev libncursesw5-dev
     pip install cibuildwheel
 elif [ "$TRAVIS_OS_NAME" = "osx" ]; then
     sudo pip install cibuildwheel
